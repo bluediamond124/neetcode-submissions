dc2f6faede40a7f00210d2ff41a5acd57feaa370
bluediamond124 <sbjohnson0000@gmail.com>
2026-07-28 08:43:24 -0500

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hashmap=Counter(nums)
+        for x in hashmap:
+            if hashmap[x]==1:
+                return x
